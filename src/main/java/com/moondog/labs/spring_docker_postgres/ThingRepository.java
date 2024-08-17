6096053e9420e210e6b65d2744e563d57d66126b
cpmooney <christopher.mooney.math@gmail.com>
2024-08-17 14:17:30 -0500

    Fix type of ID (should be UUID)

--- src/main/java/com/moondog/labs/spring_docker_postgres/ThingRepository.java
+++ src/main/java/com/moondog/labs/spring_docker_postgres/ThingRepository.java
@@ -3,6 +3,8 @@ package com.moondog.labs.spring_docker_postgres;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
-public interface ThingRepository extends CrudRepository<Thing, String> {
+public interface ThingRepository extends CrudRepository<Thing, UUID> {
 }
